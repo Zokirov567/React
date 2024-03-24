a98df37925ee33a9197689246308c81b9976bf8d
Xonjamil <xonjamil@gmail.com>
2024-03-24 15:30:36 +0500

    gg

---
+++ src/components/UI/AppLabel.tsx
@@ -0,0 +1,17 @@
+export const AppLabel = () => {
+    return (
+        <label className="input-wrapper" htmlFor="username">
+              Ваше имя
+              <input
+                required
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Ваш ответ"
+              />
+              <span id="error-message">
+                Введите имя
+              </span>
+            </label>
+    )
+}
